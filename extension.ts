b8d426cd2fc8ebe850eb048b3387957912817bde
Sebastian Wiesner <sebastian@swsnr.de>
2019-11-16 12:13:31 +0100

    Refactor to use objects and functions
    Get rid of the class with internal mutable state.
    
    Instead pass everything as argument, and register the provider only
    after we found recent projects.

--- extension.ts
+++ extension.ts
@@ -90,6 +90,36 @@ interface ProjectMap {
 }
 
 /**
+ * Lookup projects by their identifiers.
+ *
+ * @param projects Known projects
+ * @param identifiers Identifiers to look for
+ * @returns All projects from `projects` with any of the given `identifiers`.
+ */
+const lookupProjects = (
+  projects: ProjectMap,
+  identifiers: ReadonlyArray<string>
+): Project[] =>
+  identifiers
+    .filter(id => Object.prototype.hasOwnProperty.call(projects, id))
+    .map(id => projects[id]);
+
+/**
+ * Lookup a single project.
+ *
+ * @param projects Known projects
+ * @param identifier The project identifier to look for
+ * @returns The project with `identifier` or `null`
+ */
+const lookupProject = (
+  projects: ProjectMap,
+  identifier: string
+): Project | null =>
+  Object.prototype.hasOwnProperty.call(projects, identifier)
+    ? projects[identifier]
+    : null;
+
+/**
  * Whether the project matches all terms.
  *
  * Check whether the project matches all terms, by checking each term against
@@ -120,149 +150,100 @@ const findMatchingIds = (
 ): string[] =>
   projects.filter(p => projectMatchesAllTerms(p, terms)).map(p => p.id);
 
-class IDEAProvider implements SearchProvider {
-  private projects: ProjectMap;
-  public readonly appInfo: imports.gi.Gio.DesktopAppInfo;
+/**
+ * Launch IDEA or show an error notification on failure.
+ *
+ * @param idea Desktop App Info for IDEA
+ * @param files Files to launch IDEA with
+ */
+const launchIDEAInShell = (
+  idea: imports.gi.Gio.DesktopAppInfo,
+  files?: imports.gi.Gio.File[]
+): void => {
+  try {
+    idea.launch(files || [], null);
+  } catch (err) {
+    imports.ui.main.notifyError("Failed to launch IntelliJ IDEA", err.message);
+  }
+};
 
-  constructor(
-    extensionDirectory: imports.gi.Gio.File,
-    ideaApp: imports.gi.Gio.DesktopAppInfo
-  ) {
-    this.appInfo = ideaApp;
-    this.projects = {};
-    const helper = extensionDirectory.get_child("find-projects.py").get_path();
-    if (!helper) {
-      throw new Error("Helper find-projects.py doesn't exist!");
+/**
+ * Create result meta info for a project.
+ *
+ * @param idea The IDEA app info
+ * @returns A function with creates result metadata for a given project.
+ */
+const resultMetaForProject = (idea: imports.gi.Gio.DesktopAppInfo) => (
+  project: Project
+): ResultMeta => ({
+  id: project.id,
+  name: project.name,
+  description: project.path,
+  createIcon: (size): imports.gi.St.Icon | undefined => {
+    const gicon = idea.get_icon();
+    if (gicon) {
+      return new St.Icon({
+        gicon,
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        icon_size: size
+      });
+    } else {
+      return undefined;
     }
-    log(`Running Python helper ${helper} to discover IntelliJ IDEA projects`);
-    execCommand(["python3", helper]).then(
-      output => {
-        this.projects = JSON.parse(output);
-        log(`Found projects: ${Object.keys(this.projects)}`);
-      },
-      error =>
-        imports.ui.main.notifyError(
-          "Failed to find intellij projects",
-          `Couldn't run helper ${helper}: ${error.message}`
-        )
-    );
   }
+});
 
-  getInitialResultSet(
-    terms: ReadonlyArray<string>,
-    callback: (ids: string[]) => void
-  ): void {
-    callback(findMatchingIds(Object.values(this.projects), terms));
-  }
-
-  getSubsearchResultSet(
-    currentResultIDs: ReadonlyArray<string>,
-    terms: string[],
-    callback: (ids: string[]) => void
-  ): void {
-    callback(findMatchingIds(this.getProjects(currentResultIDs), terms));
-  }
-
-  getResultMetas(
-    identifiers: ReadonlyArray<string>,
-    callback: (metas: ResultMeta[]) => void
-  ): void {
-    callback(
-      this.getProjects(identifiers).map(project => ({
-        // The ID of the project as given
-        id: project.id,
-        // The project name
-        name: project.name,
-        // Use the human-readable path as description
-        description: project.path,
-        // Use the IDEA icon for each search result
-        createIcon: (size): imports.gi.St.Icon | undefined => {
-          const gicon = this.appInfo.get_icon();
-          if (gicon) {
-            return new St.Icon({
-              gicon,
-              // eslint-disable-next-line @typescript-eslint/camelcase
-              icon_size: size
-            });
-          } else {
-            return undefined;
-          }
-        }
-      }))
-    );
-  }
-
-  activateResult(identifier: string): void {
-    const project = this.getProject(identifier);
+/**
+ * Create a search provider for IDEA projects.
+ *
+ * The project exposes the given projects for search.  On activation it uses the
+ * given IDEA app to open projects.
+ *
+ * On search provider activation, that is, when the user clicks on the search
+ * provider icon to resume search in the app, it merely opens IDEA without any
+ * projects, since IDEA doesn't provide an interface start a recent projects
+ * search within IDEA.
+ *
+ * @param projects The project to search in
+ * @param idea The IntelliJ IDEA application info
+ */
+const createProvider = (
+  projects: ProjectMap,
+  idea: imports.gi.Gio.DesktopAppInfo
+): SearchProvider => ({
+  getInitialResultSet: (terms, callback): void =>
+    callback(findMatchingIds(Object.values(projects), terms)),
+  getSubsearchResultSet: (current, terms, callback): void =>
+    callback(findMatchingIds(lookupProjects(projects, current), terms)),
+  getResultMetas: (ids, callback): void =>
+    callback(lookupProjects(projects, ids).map(resultMetaForProject(idea))),
+  launchSearch: (): void => launchIDEAInShell(idea),
+  activateResult: (id: string): void => {
+    const project = lookupProject(projects, id);
     if (project) {
-      this.launchIDEA([Gio.File.new_for_path(project.abspath)]);
+      launchIDEAInShell(idea, [Gio.File.new_for_path(project.abspath)]);
     }
-  }
+  },
+  filterResults: (results, max): string[] => results.slice(0, max)
+});
 
-  /**
-   * Click on the provider icon.
-   *
-   * This function receives a list of terms like `getInitialResultSet`; the
-   * IDEA is to launch the underlying application and continue searching in
-   * the application using the given terms.
-   *
-   * However IDEA doesn't let us open a search dialog for recent projects from
-   * the outside, let alone specify search terms, so we just launch IDEA
-   * without any further arguments (just like the desktop launcher would do).
-   *
-   * Not exactly useful, but better than nothing.
-   */
-  launchSearch(): void {
-    this.launchIDEA();
+/**
+ * Find all recent projects.
+ *
+ * @param extensionDirectory The directory of this extension
+ * @returns A promise with all recent IDEA projects.
+ */
+const recentProjects = (
+  extensionDirectory: imports.gi.Gio.File
+): Promise<ProjectMap> => {
+  const helper = extensionDirectory.get_child("find-projects.py").get_path();
+  if (!helper) {
+    return Promise.reject(new Error("Helper find-projects.py doesn't exist!"));
+  } else {
+    log(`Running Python helper ${helper} to discover IntelliJ IDEA projects`);
+    return execCommand(["python3", helper]).then(output => JSON.parse(output));
   }
-
-  /**
-   * This method is an undocumented requirement by GNOME Shell.
-   */
-  filterResults(results: ReadonlyArray<string>, max: number): string[] {
-    return results.slice(0, max);
-  }
-
-  /**
-   * Launch IDEA with the given files.
-   *
-   * Catch all errors that occur and display a notification dialog for errors.
-   *
-   * @param files
-   */
-  private launchIDEA(files?: imports.gi.Gio.File[]): void {
-    try {
-      this.appInfo.launch(files || [], null);
-    } catch (err) {
-      imports.ui.main.notifyError(
-        "Failed to launch IntelliJ IDEA",
-        err.message
-      );
-    }
-  }
-
-  /**
-   * Get all projects with the given identifiers.
-   *
-   * Ignore unknown identifiers.
-   */
-  private getProjects(identifiers: ReadonlyArray<string>): Project[] {
-    return identifiers
-      .filter(id => Object.prototype.hasOwnProperty.call(this.projects, id))
-      .map(id => this.projects[id]);
-  }
-
-  /**
-   * Get the project with the given ID or null.
-   *
-   * @param identifier
-   */
-  private getProject(identifier: string): Project | null {
-    return Object.prototype.hasOwnProperty.call(this.projects, identifier)
-      ? this.projects[identifier]
-      : null;
-  }
-}
+};
 
 /**
  * Get the current extension.
@@ -275,12 +256,15 @@ const currentExtension = (): ExtensionObject => {
   return ext;
 };
 
+type RegisteredProvider = "unregistered" | "registering" | SearchProvider;
+
 /**
  * The registered provider if any.
  *
  * Only used for correctly deregistering, and to prevent registering it twice.
  */
-let registeredProvider: IDEAProvider | null = null;
+// eslint-disable-next-line immutable/no-let
+let registeredProvider: RegisteredProvider = "unregistered";
 
 /**
  * Initialize this extension immediately after loading.
@@ -297,15 +281,33 @@ function init(): void {}
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function enable(): void {
-  if (!registeredProvider) {
+  if (registeredProvider === "unregistered") {
     const me = currentExtension();
     log(`enabling ${me.metadata.name} version ${me.metadata.version}`);
     const main = imports.ui.main;
     const idea = findIDEA();
     if (idea) {
-      registeredProvider = new IDEAProvider(me.dir, idea);
-      (main.overview as any).viewSelector._searchResults._registerProvider(
-        registeredProvider
+      registeredProvider = "registering";
+      recentProjects(me.dir).then(
+        projects => {
+          if (registeredProvider === "registering") {
+            // If the user hasn't disabled the extension meanwhile create the
+            // search provider and registered it, both in our global variable
+            // and for gnome shell.
+            const provider = createProvider(projects, idea);
+            registeredProvider = provider;
+            (main.overview as any).viewSelector._searchResults._registerProvider(
+              registeredProvider
+            );
+          }
+        },
+        error => {
+          // If the the user hasn't disabled the extension meanwhile show an
+          // error message.
+          if (registeredProvider === "registering") {
+            main.notifyError("Failed to find recent projects", error.message);
+          }
+        }
       );
     } else {
       main.notifyError(
@@ -323,13 +325,16 @@ function enable(): void {
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function disable(): void {
-  if (registeredProvider) {
+  if (typeof registeredProvider !== "string") {
+    // Remove the provider if it was registered
     const me = currentExtension();
     log(`disabling ${me.metadata.name} version ${me.metadata.version}`);
     const main = imports.ui.main;
     (main.overview as any).viewSelector._searchResults._unregisterProvider(
       registeredProvider
     );
-    registeredProvider = null;
   }
+  // In any case mark the provider as unregistered, so that we can register it
+  // again when the user reenables the extension.
+  registeredProvider = "unregistered";
 }
