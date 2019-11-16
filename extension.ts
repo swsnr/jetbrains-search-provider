cef04449e63d7d2bcb84b323f83895b5c0183df0
Sebastian Wiesner <sebastian@swsnr.de>
2019-11-16 12:15:05 +0100

    Remove redundant use strict
    TSC emits this for us.

--- extension.ts
+++ extension.ts
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-"use strict";
-
 const Gio = imports.gi.Gio;
 const St = imports.gi.St;
 
